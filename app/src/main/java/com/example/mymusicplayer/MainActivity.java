1020fea8094d9e3a98aaf9e00e9262f3b67a6b61
Subham kumar <80710806+loganrv10@users.noreply.github.com>
2021-09-27 16:34:19 +0530

    music player

--- app/src/main/java/com/example/mymusicplayer/MainActivity.java
+++ app/src/main/java/com/example/mymusicplayer/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.mymusicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     ImageButton idBtnSearch;
     ProgressBar progressBar;
+    ArrayList<ResultsDTO> arrayList = new ArrayList<>();
 
 
 
@@ -23,5 +32,37 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         recyclerView = findViewById(R.id.recycleView);
+        idBtnSearch = findViewById(R.id.idBtnSearch);
+        progressBar = findViewById(R.id.progressBar);
+        idBtnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callApi();
+            }
+        });
+    }
+
+    private void callApi() {
+        Apiservice apiservice = Network.getRetrofitInstance().create(Apiservice.class);
+        apiservice.getPost("postid").enqueue(new Callback<ResultsDTO>() {
+            @Override
+            public void onResponse(Call<ResultsDTO> call, Response<ResultsDTO> response) {
+                arrayList = response.body();
+                setRecycleView();
+
+            }
+
+            @Override
+            public void onFailure(Call<ResultsDTO> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void setRecycleView() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this);
+        SongAdapter songAdapter = new SongAdapter(arrayList);
+        recyclerView.setAdapter(songAdapter);
+        recyclerView.setLayoutManager(linearLayoutManager);
     }
 }
---
+++ app/src/main/java/com/example/mymusicplayer/SongAdapter.java
@@ -0,0 +1,36 @@
+package com.example.mymusicplayer;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class SongAdapter extends RecyclerView.Adapter<SongHolder> {
+    ArrayList<ResultsDTO> arrayList = new ArrayList<ResultsDTO>();
+
+    public SongAdapter(ArrayList<ResultsDTO> arrayList) {
+        this.arrayList = arrayList;
+    }
+
+    @NonNull
+    @Override
+    public SongHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout,parent,false);
+        return new SongHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SongHolder holder, int position) {
+      ResultsDTO resultsDTO = arrayList.get(position);
+      holder.setData(resultsDTO);
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+}
